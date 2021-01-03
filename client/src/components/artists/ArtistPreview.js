9425137851c0691917515dc7d781c62680d2f7b1
bhart92 <hartwebdev92@gmail.com>
2021-01-02 22:15:09 -0800

    next upload

--- client/src/components/artists/ArtistPreview.js
+++ client/src/components/artists/ArtistPreview.js
@@ -2,33 +2,26 @@ import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
 const ArtistPreview = () => {
-    const artists = [
-        {
-            title: 'Essentials',
-            image: 'artistCard1.jpg'
-        },
-        {
-            title: 'New',
-            image: 'artistCard2.jpg'
-        },
-        {
-            title: 'Hip Hop',
-            image: 'artistCard3.jpg'
-        }];
     return (
         <Fragment>
             <div className='artists--playlist-preview'>
                     <h1>Playlists</h1>
-                 <div className='artists--card-container'>
-                     {artists.map((artist, index) => {
-                         return (
-                            <div key={index} className='artist--card-wrapper'>
-                                <h2>Get In the Know</h2>
-                                <span>{artist.title}</span>
-                                    <div className='artists--card' style={{ backgroundImage: `url('${artist.image}')` }}></div>
-                            </div>
-                         );
-                     })}
+                <div className='artists--card-container'>
+                    <div className='artist--card-wrapper'>
+                                    <h2>Get In the Know</h2>
+                                    <span>Essentials</span>
+                                        <div className='artists--card first' ></div>
+                    </div>
+                    <div className='artist--card-wrapper'>
+                                    <h2>Get In the Know</h2>
+                                    <span>New</span>
+                    <div className='artists--card second' ></div>
+                    </div>
+                    <div className='artist--card-wrapper'>
+                                    <h2>Get In the Know</h2>
+                                    <span>Hip Hop</span>
+                    <div className='artists--card third' ></div>
+                    </div>
                 </div>
             </div>
         </Fragment>
